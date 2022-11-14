24ca30256b0b09eba2381efed0bc7167b1af866c
Joseph D Prater <89654067+JOEPRATER63@users.noreply.github.com>
2022-11-14 11:47:20 -0500

    Add files via upload

---
+++ Tutorial/people.js
@@ -0,0 +1,8 @@
+const people = ['joe', 'tim', 'wes', 'abby'];
+const ages = [29, 38, 38, 37];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+}
---
+++ Tutorial/test.js
@@ -0,0 +1,9 @@
+// let names = 'joe';
+
+// console.log(names);
+
+const greet = (names) => {
+    console.log(`hello, ${names}`);
+}
+greet("joe");
+greet("mariah")
